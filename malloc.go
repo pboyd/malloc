9f25442825233f5021c056d9eeb7104375f7b6e1
Paul Boyd <paul@camelot.email>
2022-12-21 12:48:35 -0500

    Implement Free and fix bugs

--- malloc.go
+++ malloc.go
@@ -3,16 +3,12 @@ package malloc
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"reflect"
 	"unsafe"
 )
 
-// Number of bytes in each word. This is the smallest size we will allocate.
-const wordSize = 8
-
-// The size of blockHeader in words.
-const blockHeaderSize = unsafe.Sizeof((*(*blockHeader)(nil))) / wordSize
+// Number of bytes in each word.
+const wordSize = 16
 
 // ErrOutOfMemory is returned when there is insufficient available memory in
 // the Arena to satisfy the request.
@@ -20,80 +16,155 @@ var ErrOutOfMemory = errors.New("out of memory")
 
 // Arena holds a fixed amount of memory which can be allocated.
 type Arena struct {
-	buf  []uint64
-	head blockHeader
+	buf [][2]uint64
 }
 
 // NewArena makes a new arena of the given size.
 //
-// NewArena will panic if size is not a multiple of 8.
-func NewArena(size uintptr) *Arena {
-	if size%wordSize != 0 {
-		panic(fmt.Sprintf("size must be a multiple of %d", wordSize))
+// The largest theoretical size of an arena is 32 GiB.
+func NewArena(size uint64) *Arena {
+	words := uintptrToWords(uintptr(size))
+
+	a := &Arena{buf: make([][2]uint64, words)}
+
+	// Layout the initial memory:
+	// - The first free block has no space, it exists to point to the first
+	//   actual free block. Which is initially the second block
+	// - The second block has all the space which isn't in the first block.
+
+	second := a.index(1)
+	second.size = words - 1
+
+	first := a.index(0)
+	first.next = second
+
+	return a
+}
+
+// Size returns the total amount of memory (in bytes) managed by the arena.
+func (a *Arena) Size() int {
+	return len(a.buf) * wordSize
+}
+
+// Cap returns the total amount of usable memory in the arena.
+func (a *Arena) Cap() int {
+	return a.Size() - int(wordSize)
+}
+
+// FreeBytes returns the amount of unallocated space in the arena.
+//
+// This requires walking the list of free blocks, so it can be slow.
+func (a *Arena) FreeBytes() int {
+	freeWords := 0
+
+	for q := a.index(0); q != nil; q = q.next {
+		freeWords += int(q.size)
 	}
 
-	words := uint32(size / wordSize)
-
-	buf := make([]uint64, words)
-
-	// Mark the first block as having all the space.
-	first := (*blockHeader)(unsafe.Pointer(&buf[0]))
-	first.next = nil
-	first.size = words
-
-	return &Arena{
-		buf:  buf,
-		head: blockHeader{next: first},
-	}
+	return freeWords * wordSize
 }
 
 // Malloc allocates a new pointer in the arena of at least the given size. Size
 // is in bytes.
 //
-// Memory is allocated in 8-byte chunks. If size is not evenly divisible by 8
+// Memory is managed in 16-byte words. If size is not evenly divisible by 16
 // it will be rounded up.
 func (a *Arena) Malloc(size uintptr) (unsafe.Pointer, error) {
-	var words uint32
-	if size%wordSize == 0 {
-		words = uint32(size / wordSize)
-	} else {
-		words = uint32(size/wordSize + 1)
-	}
-
 	// This algorithm is based on the First-fit allocator in Knuth AOCP 1.2.5.
 
-	q := &a.head
+	words := uintptrToWords(size)
+
+	// Search through the list of free blocks looking for one big enough to hold
+	// the requested size.
+	q := a.index(0)
 	p := q.next
-
-	for p != nil {
-		if p.size < words {
-			// Doesn't fit, move on.
-			q = p
-			p = q.next
-			continue
-		}
-
-		// We can fit the
-		k := p.size - words
-
-		// Knuth says to check if k == 0. But since our header doesn't fit in one
-		// word, we'd end up giving part of our header away.
-		if uintptr(k) < blockHeaderSize {
-			q.next = p.next
-		} else {
-			p.size = k
-		}
-
-		return p.Pointer(k), nil
+	for p != nil && p.size < words {
+		q = p
+		p = q.next
 	}
 
-	return nil, ErrOutOfMemory
+	if p == nil {
+		return nil, ErrOutOfMemory
+	}
+
+	// We can fit the amount requested in p, and q is block immediately before p.
+
+	// Find the new size of the free block.
+	k := p.size - words
+
+	// Is all the space in p taken up by the new request?
+	if k == 0 {
+		q.next = p.next
+	} else {
+		p.size = k
+	}
+
+	return p.Pointer(k), nil
 }
 
+// Free deallocates size bytes of memory starting a the pointer.
+//
+// If the pointer is not inside the arena Free will panic.
+func (a *Arena) Free(x unsafe.Pointer, size uintptr) {
+	// This is the "Liberation" algorithm from Knuth 1.2.5 that goes along with
+	// the first-fit algorithm above.
+	//
+	// The idea is to convert the referenced memory to a free node (i.e. add a
+	// blockHeader) and insert it into the free list. If the block is adjacent
+	// to another free block they'll be merged together.
+	if x == nil {
+		return
+	}
+
+	words := uintptrToWords(size)
+
+	p0 := (*blockHeader)(x)
+	if !a.Contains(p0) {
+		panic("attempted to free a pointer that is not in arena")
+	}
+
+	// Search through the free list to find the entry immediately before the area
+	// to be freed, or the end of the list, whichever comes first.
+	//
+	// This assumes that later entries in the free list have greater memory
+	// addresses.
+
+	q := a.index(0)
+	p := q.next
+	for p != nil && addrOf(p) < addrOf(p0) {
+		q = p
+		p = q.next
+	}
+
+	// Now q is the entry before p0 and p is entry after it (which will be nil,
+	// if p0 is in the last block), so p0 should be inserted in the list between
+	// those two entries.
+
+	if p != nil && addrOf(p0)+uintptr(words*wordSize) == addrOf(p) {
+		// Nothing was allocated between p0 and p, so merge the two free blocks together.
+		words += p.size
+		p0.next = p.next
+	} else {
+		p0.next = p
+	}
+	if addrOf(q)+uintptr(q.size) == addrOf(p0) {
+		// Nothing was allocated between q an p0, so merge the two free blocks together.
+		q.size += words
+		q.next = p0.next
+	} else {
+		q.next = p0
+		p0.size = words
+	}
+}
+
+// Raw makes a copy of the memory for for debugging.
 func (a *Arena) Raw() []byte {
 	buf := make([]byte, len(a.buf)*wordSize)
 	for i, v := range a.buf {
-		binary.LittleEndian.PutUint64(buf[i*8:i*8+8], v)
+		offset := i * wordSize
+		binary.LittleEndian.PutUint64(buf[offset:offset+8], v[0])
+		offset += 8
+		binary.LittleEndian.PutUint64(buf[offset:offset+8], v[1])
 	}
 	return buf
 }
@@ -103,8 +174,15 @@ func (a *Arena) Raw() []byte {
 //
 // Panics if p is not a pointer.
 func (a *Arena) Contains(p any) bool {
-	addr := uintptr(reflect.ValueOf(p).UnsafePointer())
+	addr := addrOf(p)
 	return addr >= uintptr(unsafe.Pointer(&a.buf[0])) && addr <= uintptr(unsafe.Pointer(&a.buf[len(a.buf)-1]))
+}
+
+// index returns the nth blockHeader, if the entire memory were an array of
+// blockHeaders. This is generally unsafe, except that index 0 is always the
+// list head. And in an empty arena index 1 has all the space.
+func (a *Arena) index(n uint32) *blockHeader {
+	return (*blockHeader)(unsafe.Pointer(&a.buf[n]))
 }
 
 // Malloc allocates a pointer of an arbitrary type in the arena.
@@ -114,6 +192,22 @@ func Malloc[T any](a *Arena) (*T, error) {
 		return nil, err
 	}
 	return (*T)(p), nil
+}
+
+// Free deallocates the memory associated with a pointer that was previously
+// allocated in the arena.
+//
+// Free will panic if the pointer was not allocated within the arena.
+func Free[T any](a *Arena, p *T) {
+	a.Free(unsafe.Pointer(p), unsafe.Sizeof((*(*T)(nil))))
+}
+
+func uintptrToWords(s uintptr) uint32 {
+	if s%wordSize == 0 {
+		return uint32(s / wordSize)
+	}
+
+	return uint32(s/wordSize + 1)
 }
 
 // blockHeader marks the beginning of each free block.
@@ -129,4 +223,8 @@ type blockHeader struct {
 // Pointer returns an unsafe.Pointer to the nth word in the block.
 func (b *blockHeader) Pointer(n uint32) unsafe.Pointer {
 	return unsafe.Pointer(uintptr(unsafe.Pointer(b)) + uintptr(n*wordSize))
+}
+
+func addrOf(p any) uintptr {
+	return uintptr(reflect.ValueOf(p).UnsafePointer())
 }
--- malloc_test.go
+++ malloc_test.go
@@ -1,7 +1,9 @@
 package malloc
 
 import (
+	"math/rand"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,9 +11,9 @@ import (
 func TestMallocAll(t *testing.T) {
 	assert := assert.New(t)
 
-	// Allocate everything in the arena in a single array.
+	// Allocate everything in the arena (after the header) in a single array.
 	a := NewArena(64)
-	p, err := Malloc[[64]byte](a)
+	p, err := Malloc[[64 - wordSize]byte](a)
 	if !assert.NoError(err) {
 		return
 	}
@@ -21,33 +23,70 @@ func TestMallocAll(t *testing.T) {
 	for i := 0; i < len(*p); i++ {
 		(*p)[i] = byte(i)
 	}
-	for i, b := range a.Raw() {
+	for i, b := range a.Raw()[wordSize:] {
 		assert.Equal(byte(i), b)
 	}
 
-	// Finally verify that no more memory can be allocated.
+	// Verify that no more memory can be allocated.
 	overflow, err := Malloc[byte](a)
 	assert.Nil(overflow)
 	assert.Error(err, ErrOutOfMemory)
+
+	Free(a, p)
+	assert.Equal(a.FreeBytes(), a.Cap())
 }
 
-func TestMallocAllInts(t *testing.T) {
+func TestMallocSmallItems(t *testing.T) {
 	assert := assert.New(t)
 
-	a := NewArena(64)
+	a := NewArena(10 * 1024)
 
-	// We can only get 7 words with the current algorithm due to waste.
-	for i := 0; i < 7; i++ {
-		p, err := Malloc[uint64](a)
-		if !assert.NoError(err) {
-			return
+	allocAll := func() []*uint64 {
+		// It's really innefficient for this algorithm to store 8-byte values,
+		// because the smallest value it can handle is 16 bytes. So we end up
+		// wasting over half the space.
+		pointers := make([]*uint64, a.Cap()/16)
+		for i := range pointers {
+			var err error
+			pointers[i], err = Malloc[uint64](a)
+			if !assert.NoError(err) {
+				return pointers
+			}
+			*pointers[i] = ^uint64(0)
 		}
-		*p = ^uint64(0)
+
+		overflow, err := Malloc[byte](a)
+		assert.Nil(overflow)
+		assert.Error(err, ErrOutOfMemory)
+
+		return pointers
 	}
 
-	overflow, err := Malloc[byte](a)
-	assert.Nil(overflow)
-	assert.Error(err, ErrOutOfMemory)
+	// Allocate everything and make sure it's full.
+	pointers := allocAll()
+	assert.Equal(a.FreeBytes(), 0)
+
+	// Now free everything.
+	for _, p := range pointers {
+		Free(a, p)
+	}
+
+	assert.Equal(a.FreeBytes(), a.Cap())
+
+	// Now allocate it all again.
+	pointers = allocAll()
+	assert.Equal(a.FreeBytes(), 0)
+
+	// Free it all again, but this free it in a random order.
+	rnd := rand.New(rand.NewSource(1))
+	for len(pointers) > 0 {
+		n := rnd.Intn(len(pointers))
+		p := pointers[n]
+		pointers = append(pointers[:n], pointers[n+1:]...)
+		Free(a, p)
+	}
+
+	assert.Equal(a.FreeBytes(), a.Cap())
 }
 
 func TestMallocStruct(t *testing.T) {
@@ -84,4 +123,96 @@ func TestMallocFunction(t *testing.T) {
 
 	assert.True(a.Contains(double))
 	assert.Equal(4, (*double)(2))
+}
+
+func TestRandomMallocs(t *testing.T) {
+	assert := assert.New(t)
+
+	ra := newRandomAllocator(rand.NewSource(0), 10*1024)
+
+	assert.Equal(ra.Arena.FreeBytes(), ra.Arena.Cap())
+
+	for i := 1; i <= 10000; i++ {
+		switch {
+		case i%2 == 0:
+			ra.Malloc(64)
+		case i%5 == 0:
+			ra.Free(1)
+		case i%100 == 0:
+			ra.Malloc(10000)
+		}
+	}
+
+	ra.FreeAll()
+	assert.Equal(ra.Arena.FreeBytes(), ra.Arena.Cap())
+}
+
+func BenchmarkMallocAndFree(b *testing.B) {
+	assert := assert.New(b)
+	ra := newRandomAllocator(rand.NewSource(0), 1024*1024)
+	b.ResetTimer()
+
+	for i := 1; i <= b.N; i++ {
+		switch {
+		case i%2 == 0:
+			ra.Malloc(64)
+		case i%5 == 0:
+			ra.Free(1)
+		case i%100 == 0:
+			ra.Malloc(10000)
+		}
+	}
+
+	ra.FreeAll()
+	assert.Equal(ra.Arena.FreeBytes(), ra.Arena.Cap())
+}
+
+type randomAllocator struct {
+	rnd     *rand.Rand
+	records []allocRecord
+	Arena   *Arena
+}
+
+type allocRecord struct {
+	pointer unsafe.Pointer
+	size    uintptr
+}
+
+func newRandomAllocator(source rand.Source, arenaSize uint64) *randomAllocator {
+	return &randomAllocator{
+		rnd:     rand.New(source),
+		records: []allocRecord{},
+		Arena:   NewArena(arenaSize),
+	}
+}
+
+func (ra *randomAllocator) Malloc(max int) bool {
+	size := uintptr(ra.rnd.Intn(max-1) + 1)
+	p, err := ra.Arena.Malloc(size)
+	if err != nil {
+		return false
+	}
+
+	ra.records = append(ra.records, allocRecord{
+		pointer: p,
+		size:    size,
+	})
+
+	return true
+}
+
+// Free takes n random entries from the arena and frees them.
+func (ra *randomAllocator) Free(n int) {
+	for ; n > 0 && len(ra.records) > 0; n-- {
+		i := ra.rnd.Intn(len(ra.records))
+		ra.Arena.Free(ra.records[i].pointer, ra.records[i].size)
+		ra.records = append(ra.records[:i], ra.records[i+1:]...)
+	}
+}
+
+func (ra *randomAllocator) FreeAll() {
+	for _, rec := range ra.records {
+		ra.Arena.Free(rec.pointer, rec.size)
+	}
+	ra.records = ra.records[:0]
 }
